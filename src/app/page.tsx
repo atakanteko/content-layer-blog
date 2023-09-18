6bdbcc1c659be738b639f91b526b386fd7134226
atakan.tekoglu <atakan.tekoglu@param.com.tr>
2023-09-18 16:56:22 +0300

    feature: dashboard hero mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Featured from '@/components/home/featured/featured';
-import Hero from '@/components/hero/hero';
+import Hero from '@/components/home/hero/hero';
 
 export default function Home() {
   return (
