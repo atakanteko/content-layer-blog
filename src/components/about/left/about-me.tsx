899f41c6422af78a9ed3a18c33feb4d8f954776d
atakan.tekoglu <atakan.tekoglu@param.com.tr>
2023-10-16 21:20:21 +0300

    improve: about me

--- src/components/about/left/about-me.tsx
+++ src/components/about/left/about-me.tsx
@@ -6,12 +6,16 @@ const AboutMeDesc = () => {
     <div className={styles.about_me}>
       <h2>About Me</h2>
       <p>
-        Use this area to say something about yourself or describe your goals.
-        Tum dicere exorsus est cur verear, ne ad id omnia referri oporteat,
-        ipsum per se ipsam voluptatem, quia consequuntur magni dolores eos, qui
-        blanditiis praesentium voluptatum deleniti atque insitam in ea quid est
-        eligendi optio, cumque nihil ut ipsi auctori huius disciplinae placet:
-        constituam, quid.
+        My name is Atakan. {`I'm`} a Frontend Developer, having a degree in
+        Computer Engineering. Currently, I live in Istanbul (Turkiye). My
+        passion is web applications development: programming, automated testing,
+        and good application architecture. {`I'm`} skilled at JavaScript,
+        TypeScript, HTML, CSS, including React.js and a bit Vue.js frameworks. I
+        have good communication skills and interpersonal skills. I strive to be
+        a humble team player and be approachable to receive and give feedback.
+        On this blog, I publish regularly posts about Frontend development,
+        mainly about JavaScript, TypeScript, Vue.js, and React.js. Writing helps
+        me sharpen my writing skills and learn a lot along the way.
       </p>
     </div>
   );
